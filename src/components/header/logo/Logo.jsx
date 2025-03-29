ab590a50c4865012d319dd817c5327e72647b187
Akuchinyerenwa OPUS <littlepetero@gmail.com>
2025-03-29 18:50:41 +0100

    increased button font weight and letter spacing

--- src/components/header/logo/Logo.jsx
+++ src/components/header/logo/Logo.jsx
@@ -3,6 +3,8 @@ import "./Logo.css"
 export const Logo = () => {
 
   return (
-    <img className="logo-root" src="/images/header/logo.png" alt="Interncity hub logo" />
+   <a href="/">
+     <img className="logo-root" src="/images/header/logo.png" alt="Interncity hub logo" />
+   </a>
   )
 }
