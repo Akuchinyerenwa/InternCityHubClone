c40a5c858af6b07f9e166cfcd22a904f8d8e39a9
Akuchinyerenwa OPUS <littlepetero@gmail.com>
2025-03-29 16:50:03 +0100

    styled course component

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -7,6 +7,10 @@ export const HomePage = () => {
   return(
     <div className="home-page-root">
       <Course title="Front-End" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis autem veniam nemo dolorum, vitae, deserunt repellendus ipsam sed veritatis odit aliquam ut pariatur tempora asperiores quaerat nobis aspernatur commodi. Fuga." />
+
+      <Course title="Front-End" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis autem veniam nemo dolorum, vitae, deserunt repellendus ipsam sed veritatis odit aliquam ut pariatur tempora asperiores quaerat nobis aspernatur commodi. Fuga." />
+
+      <Course title="Front-End" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis autem veniam nemo dolorum, vitae, deserunt repellendus ipsam sed veritatis odit aliquam ut pariatur tempora asperiores quaerat nobis aspernatur commodi. Fuga." />
     </div>
   )
 }
