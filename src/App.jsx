d5390fa5d1661ba63127cfd57950bcd1e410b2d7
Akuchinyerenwa OPUS <littlepetero@gmail.com>
2025-03-29 18:01:22 +0100

    installed react router; set up app routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
 import { Header } from "./components/header/Header";
 import { HomePage } from "./pages/home-page/HomePage";
+import { RegisterPage } from "./pages/register-page/RegisterPage";
 
 export default function App() {
   
   return (
     <div className="app-root">
       <Header />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/register" element={<RegisterPage />}/>
+      </Routes>
+      
     </div>
   );
 }
---
+++ src/pages/register-page/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import "./RegisterPage.css";
+
+export const RegisterPage = () => {
+
+
+  return (
+    <div className="register-page-root">
+      <h3>I</h3>
+    </div>
+  )
+}
