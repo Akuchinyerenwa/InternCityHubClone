fc1945175b7fa75bf23c561c39ed1b30d42d97ff
Akuchinyerenwa OPUS <littlepetero@gmail.com>
2025-03-29 18:34:46 +0100

    implemented register page and form

--- src/pages/register-page/RegisterPage.jsx
+++ src/pages/register-page/RegisterPage.jsx
@@ -1,3 +1,4 @@
+import { Button } from "../../components/general/button/Button";
 import { Input } from "../../components/general/input/Input";
 import "./RegisterPage.css";
 
@@ -6,9 +7,23 @@ export const RegisterPage = () => {
 
   return (
     <div className="register-page-root">
-      <h3>Register page</h3>
+      <h1>Register page</h1>
       <form>
-        <Input type="text" name="firstName" title="First Name" onChange={(value)=>{console.log({value})}}/>
+        <div className="double-item-container">
+          <Input type="text" name="firstName" title="First Name" onChange={(value)=>{console.log({value})}}/>
+          <Input type="text" name="lastName" title="Last Name" onChange={(value)=>{console.log({value})}}/>
+        </div>
+
+        <div className="double-item-container">
+          <Input type="text" name="track" title="Track" onChange={(value)=>{console.log({value})}}/>
+          <Input type="text" name="userName" title="Username" onChange={(value)=>{console.log({value})}}/>
+        </div>
+
+        <div className="double-item-container">
+          <Input type="email" name="email" title="Email" onChange={(value)=>{console.log({value})}}/>
+          <Input type="tel" name="phoneNumber" title="Phone Number" onChange={(value)=>{console.log({value})}}/>
+        </div>
+        <Button title="Submit"/>
       </form>
     </div>
   )
