c28fc63b1b1f286fbfa1bfc895c823449c1dcb9c
Akuchinyerenwa OPUS <littlepetero@gmail.com>
2025-03-29 18:45:59 +0100

    completed register page

--- src/components/general/input/Input.jsx
+++ src/components/general/input/Input.jsx
@@ -1,12 +1,12 @@
 import "./Input.css";
 
 
-export const Input = ({title, type, onChange, name}) => {
+export const Input = ({title, type, onChange, name, defaultValue}) => {
 
   return(
     <div className="input-root">
       <label htmlFor={name}>{title}</label>
-      <input id={name} name={name} type={type} onChange={(e)=>onChange(e.target.value)} />
+      <input id={name} name={name} type={type} defaultValue={defaultValue} onChange={(e)=>onChange(e.target.value)} />
     </div>
   )
 }
--- src/pages/register-page/RegisterPage.jsx
+++ src/pages/register-page/RegisterPage.jsx
@@ -1,27 +1,29 @@
+import { useSearchParams } from "react-router";
 import { Button } from "../../components/general/button/Button";
 import { Input } from "../../components/general/input/Input";
 import "./RegisterPage.css";
 
 export const RegisterPage = () => {
 
+  const [searchParams] = useSearchParams();
 
   return (
     <div className="register-page-root">
       <h1>Register page</h1>
       <form>
         <div className="double-item-container">
-          <Input type="text" name="firstName" title="First Name" onChange={(value)=>{console.log({value})}}/>
-          <Input type="text" name="lastName" title="Last Name" onChange={(value)=>{console.log({value})}}/>
+          <Input type="text" name="firstName" title="First Name" onChange={(value)=>{}}/>
+          <Input type="text" name="lastName" title="Last Name" onChange={(value)=>{}}/>
         </div>
 
         <div className="double-item-container">
-          <Input type="text" name="track" title="Track" onChange={(value)=>{console.log({value})}}/>
-          <Input type="text" name="userName" title="Username" onChange={(value)=>{console.log({value})}}/>
+          <Input type="text" defaultValue={searchParams.get("name")} name="track" title="Track" onChange={(value)=>{}}/>
+          <Input type="text" name="userName" title="Username" onChange={(value)=>{}}/>
         </div>
 
         <div className="double-item-container">
-          <Input type="email" name="email" title="Email" onChange={(value)=>{console.log({value})}}/>
-          <Input type="tel" name="phoneNumber" title="Phone Number" onChange={(value)=>{console.log({value})}}/>
+          <Input type="email" name="email" title="Email" onChange={(value)=>{}}/>
+          <Input type="tel" name="phoneNumber" title="Phone Number" onChange={(value)=>{}}/>
         </div>
         <Button title="Submit"/>
       </form>
